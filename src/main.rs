f5ecba5f71f537ab585858804a29a82c5e57d145
Abhi Vaidyanatha <avaidyanatha@ucsb.edu>
2020-10-08 14:48:00 -0700

    Update crate name in main.rs (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use dotenv::dotenv;
 use std::io::Write;
 use std::process;
-extern crate customer_writer;
+extern crate transmission;
 use structopt::StructOpt;
 
 fn main() {
     dotenv().ok();
 
-    let config = customer_writer::options::Opts::from_args();
+    let config = transmission::options::Opts::from_args();
 
-    if let Err(ref e) = customer_writer::run(config) {
+    if let Err(ref e) = transmission::run(config) {
         let stderr = &mut ::std::io::stderr();
         let errmsg = "Error writing to stderr";
 
