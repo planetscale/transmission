9af88038ac667b3a56acb31eda00be41a04e0935
avaidyanatha <avaidyanatha@ucsb.edu>
2020-10-08 14:31:32 -0700

    Remove old shell script artifacts.

--- get-master.sh
+++
@@ -1,5 +0,0 @@
-echo -e '\e[92mGetting master alias from shard record'
-echo -e '\e[39m'
-MASTERALIAS=$(psk peter-demo gcp uswest1 get vts libellis-doggers-puppers-x-x-4bc744fa -o yaml | rg masterAlias | sed 's/masterAlias: //g')
-echo -e "\e[92mMaster Alias is:$MASTERALIAS"
-echo -e '\e[39m'
